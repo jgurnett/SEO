dad0d9ee29162f2943f8c1580e18bd9cdf6e8a54
jgurnett <gurnettt@mymacewan.ca>
2020-11-17 16:43:21 -0700

    updates to export to an excel sheet

--- keywordSearch.py
+++ keywordSearch.py
@@ -1,5 +1,7 @@
 import getopt, sys
 from googleapi import google
+from openpyxl import load_workbook
+from datetime import date
 
 def main(argv):
 	inputfile = ""
@@ -19,9 +21,18 @@ def main(argv):
 		elif opt in ('-s', '--site'):
 			site = arg
 			print('site file is: ' + site)
+	
+	workbook = load_workbook(filename="sample.xlsx")
+	sheet = workbook.active
 	query = ""
 	file = open(inputfile, 'r')
 	Lines = file.readlines()
+	row = 2
+	col = 2
+	while sheet.cell(row=1, column=col).value != None:		
+		col = col + 1
+	sheet.cell(row=row-1, column=col).value = date.today().strftime("%Y/%m/%d") 
+	
 	for line in Lines:
 		if '\n' in line:
 			query = line.replace("\n", "")
@@ -29,6 +40,7 @@ def main(argv):
 		numPage = 3
 		print("searching for: " + query + "...")
 		searchResults = google.search(query, numPage)
+		sheet['A'+str(row)] = query
 		count = 0
 		for result in searchResults:
 			count = count + 1
@@ -39,15 +51,18 @@ def main(argv):
 					if count > 9:
 						page = int(count / 10) + 1 
 						position = count % 10
+					sheet.cell(row=row, column=col).value = count
 					standing[query] = "Page: " + str(page) + " position: " + str(position)
 					print(standing[query])
 					print()
+					break
 			except:
 				print("error!")
 		if query not in standing:
+			sheet.cell(row=row, column=col).value = 'Not Found'
 			print('Query: ' + query + " not found!\n")
 
-	print("Results :" + str(standing))
+	workbook.save(filename="sample.xlsx")
 
 
 if __name__ == "__main__":
